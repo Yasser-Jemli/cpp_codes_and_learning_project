8d95c66c506bfb2d060f3613bd9d5ea4eda29fa4
yasser-jemli <yasserjamli37@gmail.com>
2024-01-12 12:11:31 +0100

    adding a Code for Create and write to a file
    Signed-off-by: yasser-jemli <yasserjamli37@gmail.com>

---
+++ Create and Write To a File.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+  // Create and open a text file
+  ofstream MyFile("filename.txt");
+
+  // Write to the file
+  MyFile << "Files can be tricky, but it is fun enough!";
+
+  // Close the file
+  MyFile.close();7
+// Why do we close the file?
+// => It is considered good practice, and it can clean up unnecessary memory space.
+
+}
